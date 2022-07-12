6e540b800b3c9c1f35f5787485c80138b46487b9
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-07-12 20:34:37 -0300

    Arrumando requisitos 6 e 7

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,6 @@
 const saveCartItems = (arg) => {
   if (!arg) return new Error('Parâmetro necessário!');
-  localStorage.setItem('cartItems', arg);
+  localStorage.setItem('cartItems', JSON.stringify(arg));
 };
 
 if (typeof module !== 'undefined') {
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,24 +1,25 @@
-const localStorageSimulator = require('../mocks/localStorageSimulator');
-const saveCartItems = require('../helpers/saveCartItems');
+const localStorageSimulator = require("../mocks/localStorageSimulator");
+const saveCartItems = require("../helpers/saveCartItems");
 
-localStorageSimulator('setItem');
+localStorageSimulator("setItem");
 
-describe('3 - Teste a função saveCartItems', () => {
-  it('Testa se ao executar saveCartItems com argumento o método localStorage.setItem é chamado', () => {
+describe("3 - Teste a função saveCartItems", () => {
+  it("Testa se ao executar saveCartItems com argumento o método localStorage.setItem é chamado", () => {
     const myObj = {
-      key: 'valor',
-    }
+      key: "valor",
+    };
     saveCartItems(myObj);
     expect(localStorage.setItem).toHaveBeenCalled();
   });
   it('Testa se ao executar saveCartItems com argumento o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
-    const myObj = {
-      key: 'valor',
-    }
+    const myObj = { key: "valor" };
     saveCartItems(myObj);
-    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', myObj);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "cartItems",
+      '{"key":"valor"}'
+    );
   });
-  it('Testa se ao chamar saveCartItems sem argumento retorna um erro', () => {
-    expect(saveCartItems()).toEqual(new Error('Parâmetro necessário!'));
+  it("Testa se ao chamar saveCartItems sem argumento retorna um erro", () => {
+    expect(saveCartItems()).toEqual(new Error("Parâmetro necessário!"));
   });
 });
