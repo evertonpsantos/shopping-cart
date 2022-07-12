04979534fdd23a21cfd6b2ff5aab1d654eaef245
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-07-12 19:54:25 -0300

    Requisito 7

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,5 +1,6 @@
-const getSavedCartItems = () => {
-  // seu código aqui
+const getSavedCartItems = (arg) => {
+  if (!arg) return new Error('Parâmetro necessário!');
+  localStorage.getItem(arg);
 };
 
 if (typeof module !== 'undefined') {
